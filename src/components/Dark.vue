f525cb9ed9f0aecd68cf8a34af7dd28ebb993b39
LoTwT <709937065@qq.com>
2022-03-20 17:14:06 +0800

    feat: base tree

--- src/components/Dark.vue
+++ src/components/Dark.vue
@@ -3,19 +3,10 @@ import { isDark, toggleDark } from "~/composables"
 </script>
 
 <template>
-  <nav text-xl mt-6 inline-flex gap-2>
+  <div top-4 right-4 position-fixed>
     <button class="icon-btn !outline-none" @click="toggleDark()">
       <div v-if="isDark" i-carbon-moon />
       <div v-else i-carbon-sun />
     </button>
-
-    <a
-      class="icon-btn"
-      i-carbon-logo-github
-      rel="noreferrer"
-      href="https://github.com/antfu/vitesse-lite"
-      target="_blank"
-      title="GitHub"
-    />
-  </nav>
+  </div>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,47 +1,93 @@
 <script setup lang="ts">
-const name = $ref("")
+const el = $ref<HTMLCanvasElement>()
+const ctx = $computed(() => el.getContext("2d")!)
 
-const router = useRouter()
-const go = () => {
-  if (name) router.push(`/hi/${encodeURIComponent(name)}`)
+const WIDTH = $ref(600)
+const Height = $ref(600)
+
+interface Point {
+  x: number
+  y: number
 }
+
+interface Branch {
+  /**
+   * 起点
+   */
+  start: Point
+  /**
+   * 线长
+   */
+  length: number
+  /**
+   * 夹角
+   */
+  theta: number
+  /**
+   * 线宽
+   */
+  width?: number
+}
+
+function init() {
+  ctx.strokeStyle = "rgba(169, 169, 169, 0.8)"
+
+  step({
+    start: {
+      x: WIDTH / 2,
+      y: Height,
+    },
+    length: 50,
+    theta: -Math.PI / 2,
+  })
+}
+
+function lineTo(p1: Point, p2: Point) {
+  ctx.beginPath()
+  ctx.moveTo(p1.x, p1.y)
+  ctx.lineTo(p2.x, p2.y)
+  ctx.stroke()
+}
+
+function step(b: Branch) {
+  const end = getEndPoint(b)
+  drawBranch(b)
+
+  if (Math.random() < 0.5) {
+    step({
+      start: end,
+      length: b.length,
+      theta: b.theta - 0.2,
+    })
+  }
+
+  if (Math.random() < 0.5) {
+    step({
+      start: end,
+      length: b.length,
+      theta: b.theta + 0.2,
+    })
+  }
+}
+
+function getEndPoint(b: Branch) {
+  return {
+    x: b.start.x + b.length * Math.cos(b.theta),
+    y: b.start.y + b.length * Math.sin(b.theta),
+  }
+}
+
+function drawBranch(b: Branch) {
+  lineTo(b.start, getEndPoint(b))
+}
+
+onMounted(() => {
+  init()
+})
 </script>
 
 <template>
-  <div>
-    <div i-carbon-campsite text-4xl inline-block />
-    <p>
-      <a
-        rel="noreferrer"
-        href="https://github.com/antfu/vitesse-lite"
-        target="_blank"
-      >
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      placeholder="What's your name?"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    />
-
-    <div>
-      <button class="m-3 text-sm btn" :disabled="!name" @click="go">Go</button>
-    </div>
-  </div>
+  <canvas ref="el" width="600" height="600" border />
 </template>
+
+<style scoped></style>
